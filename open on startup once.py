7dd5cd24a8628512a2ee3f88e21224e2d7f438f0
BlueSpinney <99137204+BlueSpinney@users.noreply.github.com>
2022-03-29 17:17:22 +0200

    v1.1

--- open on startup once.py
+++ open on startup once.py
@@ -4,6 +4,7 @@ from datetime import date
 today = date.today()
 today = str(today)
 
+# add path to folder here     |
 os.chdir(r'C:\Users\jerome\Desktop\please fix this for me folder')
 
 
@@ -15,4 +16,5 @@ with open('daysave.txt' , 'r') as save_file:
 with open('daysave.txt' , 'w') as save_file:
     save_file.write(today)
     
+# add path and name of file you want to open here   path |                       name |      exit|
 os.system(r'cmd /k "cd C:\Users\jerome\Desktop\please fix this for me folder && t_t_d.txt && exit"')
