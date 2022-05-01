97bd94c3ec3fe5338601b5f74ce3a6eb834088b3
BlueSpinney <99137204+BlueSpinney@users.noreply.github.com>
2022-05-01 18:51:14 +0200

    v1

---
+++ own-methodes/split.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+
+main = Tk()
+
+num_and_for = []
+Oplace = 0
+opt = False
+operators = ["+", "-", "*", "/"]
+calcl = []
+empt_pos = {
+    
+}
+
+def calculate(stri):
+    global Oplace,opt,num_and_for
+    empty = 0
+    print(f"the formula is : {str} ")
+    for i in range(len(stri)):
+        if stri[i] == " ":
+            empty += 1
+            num_and_for.append(stri[Oplace:i])
+            empt_pos["empt space " + str(empty)] = [i - 1,i + 1]
+            Oplace = i
+    print(f"full list of empty spaces in String : {empt_pos}")
+    print(f"full list of words in String :  {num_and_for}")
+    Oplace = 0
+    num_and_for = []
+
+        
+
+e1 = Entry(main,bd=1)
+b1 = Button(main,text="calculate",command= lambda : calculate(stri=e1.get() + " "))
+
+e1.pack()
+b1.pack()
+main.mainloop()
