a21ffcd4b852c30d946827bcfed112e6fef0f7d5
BlueSpinney <99137204+BlueSpinney@users.noreply.github.com>
2022-05-14 21:17:49 +0200

    v2

--- Serialcom/turret/turret.ino
+++ Serialcom/turret/turret.ino
@@ -1,3 +1,9 @@
+#include <Servo.h>
+
+Servo serv1;
+Servo serv2;
+
+
 int recorx = 0;
 int recory = 0;
 
@@ -6,10 +12,15 @@ int cory = 0;
 
 int count = 0;
 
+int servox = 0;
+int servoy = 0;
 
 void setup() {
   Serial.begin(112500);
   Serial.setTimeout(1);
+
+  serv1.attach(9);
+  serv2.attach(10);
 
 }
 
@@ -17,11 +28,13 @@ void loop() {
   while (!Serial.available()){
     
   }
-  if (count == 0){
-    for (int i = 0;i < Serial.readString().toInt();i++){
-      
-    }
+  for (int i = 0;i < Serial.readString().toInt() - servox;i++){
+    serv1.write(servox + i);
+    servox = servox + i;
   }
-  
-
+  delay(500);
+  for (int i = 0;i < Serial.readString().toInt() - servoy;i++){
+    serv2.write(servox + i);
+    servoy = servoy + i;
+  }  
 }
